7e6f39ddf3887d862f408f5c6a7f70b64f35e970
s13012 <s13012@std.it-college.ac.jp>
2014-08-07 19:40:30 +0900

    ゲーム画面への画面遷移実装

--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/MainActivity.java
@@ -1,19 +1,20 @@
 package jp.ac.it_college.std.s13012.androidchallenge;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 
-
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +30,12 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -49,7 +46,7 @@ public class MainActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment{
 
         public PlaceholderFragment() {
         }
@@ -58,6 +55,16 @@ public class MainActivity extends Activity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            Button startButton = (Button)rootView.findViewById(R.id.start_button);
+            startButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getActivity(), PlayGameActivity.class);
+                    startActivity(intent);
+                }
+            });
+
             return rootView;
         }
     }
---
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/PlayGameActivity.java
@@ -0,0 +1,24 @@
+package jp.ac.it_college.std.s13012.androidchallenge;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by s13012 on 14/08/07.
+ */
+public class PlayGameActivity extends Activity{
+    public PlayGameActivity(){
+        Log.v("PlayGame", "PlayGame");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_main);
+    }
+}
