5b6d787d3184e60d071edac0a37c2072fa951774
Akinori Maesoko <s13012@std.it-college.ac.jp>
2014-08-08 22:01:15 +0900

    難易度選択追加、戻るボタン押下時のダイアログ表示追加

--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/MainActivity.java
@@ -1,71 +1,121 @@
 package jp.ac.it_college.std.s13012.androidchallenge;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+       /* if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
+        }*/
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment{
-
-        public PlaceholderFragment() {
-        }
+    public static class PlaceholderFragment extends Fragment {
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            Button startButton = (Button)rootView.findViewById(R.id.start_button);
+            final Button startButton = (Button) rootView.findViewById(R.id.start_button);
             startButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(getActivity(), PlayGameActivity.class);
-                    startActivity(intent);
+                    DifficultyFragment fragment = DifficultyFragment.newInstance();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.start_fragment, fragment);
+                    ft.commit();
+                    startButton.setVisibility(View.INVISIBLE);
                 }
             });
 
             return rootView;
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == event.KEYCODE_BACK) {
+            new AlertDialog.Builder(this)
+                    .setTitle("終了の確認")
+                    .setMessage("Tetrisを終了しますか？")
+                    .setPositiveButton("はい",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    finish();
+                                }
+                            }
+                    )
+                    .setNegativeButton("キャンセル",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                }
+                            }
+                    )
+                    .show();
+            return true;
+        }
+        return false;
+    }
+
+    public static class DifficultyFragment extends Fragment {
+        public static DifficultyFragment newInstance() {
+            DifficultyFragment fragment = new DifficultyFragment();
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_difficulty, container, false);
+
+            Button easyButton = (Button) rootView.findViewById(R.id.easy_button);
+            Button normalButton = (Button) rootView.findViewById(R.id.normal_button);
+            Button hardButton = (Button) rootView.findViewById(R.id.hard_button);
+
+            easyButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    difficultySelect("EASY");
+                }
+            });
+            normalButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    difficultySelect("NORMAL");
+                }
+            });
+            hardButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    difficultySelect("HARD");
+                }
+            });
+            return rootView;
+        }
+
+        private void difficultySelect(String difficulty) {
+            Intent intent = new Intent(getActivity(), PlayGameActivity.class);
+            intent.putExtra("Difficulty", difficulty);
+            startActivity(intent);
         }
     }
 }
--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/PlayGameActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/PlayGameActivity.java
@@ -1,22 +1,15 @@
 package jp.ac.it_college.std.s13012.androidchallenge;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 
 public class PlayGameActivity extends Activity{
-    public PlayGameActivity(){
-        Log.v("PlayGame", "PlayGame");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_main);
+        setContentView(R.layout.activity_game);
+        Log.v("PlayGame", getIntent().getStringExtra("Difficulty"));
     }
 }
