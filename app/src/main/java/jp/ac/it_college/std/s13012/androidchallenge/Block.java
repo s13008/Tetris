990fe03e0079a85016763d3bb17dad2dbc17c860
s13012 <s13012@std.it-college.ac.jp>
2014-08-14 19:59:00 +0900

    ブロックの当たり判定メソッド(check)実装 マップの外枠用にmapWidthとmapHeightを変更

--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/Block.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/Block.java
@@ -16,36 +16,36 @@ import java.util.Random;
 
 
 public class Block extends SurfaceView implements GestureDetector.OnGestureListener,
-        SurfaceHolder.Callback, Runnable{
+        SurfaceHolder.Callback, Runnable, GestureDetector.OnDoubleTapListener {
     int[][][] blocks = {
             {
-                    {1,1},
-                    {0,1},
-                    {0,1}
+                    {1, 1},
+                    {0, 1},
+                    {0, 1}
             },
             {
-                    {1,1},
-                    {1,0},
-                    {1,0}
+                    {1, 1},
+                    {1, 0},
+                    {1, 0}
             },
             {
-                    {1,1},
-                    {1,1}
+                    {1, 1},
+                    {1, 1}
             },
             {
-                    {1,0},
-                    {1,1},
-                    {1,0}
+                    {1, 0},
+                    {1, 1},
+                    {1, 0}
             },
             {
-                    {1,0},
-                    {1,1},
-                    {0,1}
+                    {1, 0},
+                    {1, 1},
+                    {0, 1}
             },
             {
-                    {0,1},
-                    {1,1},
-                    {1,0}
+                    {0, 1},
+                    {1, 1},
+                    {1, 0}
             },
             {
                     {1},
@@ -58,47 +58,41 @@ public class Block extends SurfaceView implements GestureDetector.OnGestureListe
     Random mRand = new Random(System.currentTimeMillis());
 
     int[][] block = blocks[mRand.nextInt(blocks.length)];
-    int mapWidth  = 10;
-    int mapHeight = 20;
-    public int posx, posy;
+    int mapWidth = 12;
+    int mapHeight = 21;
+    public int posx = mapWidth / 2, posy;
     int[][] map = new int[mapHeight][];
-    private final static int BLOCK_SIZE = 80;
+    private final static int BLOCK_SIZE = 53;
     GestureDetector gestureDetector;
     static Thread mThread;
     private SurfaceHolder mHolder;
     static boolean mIsAttached;
     Canvas mCanvas = null;
-    Paint mPaint = null;
-
+    static int fallVelocity;
 
 
     public Block(Context context) {
         super(context);
-        gestureDetector = new GestureDetector(context,this);
+        gestureDetector = new GestureDetector(context, this);
         mHolder = getHolder();
         mHolder.addCallback(this);
-        Log.v("Constructor","true");
+        initGame();
     }
 
-    private void drawBlock(int[][] matrix, int offsetx, int offsety, int color) {
+    private void drawMatrix(int[][] matrix, int offsetx, int offsety, int color) {
         ShapeDrawable rect = new ShapeDrawable(new RectShape());
         rect.getPaint().setColor(color);
-//        mPaint.setColor(color);
-//        mPaint.setStyle(Paint.Style.STROKE);
-        mCanvas = getHolder().lockCanvas();
-        mCanvas.drawColor(Color.BLACK);
 
         int h = matrix.length;
         int w = matrix[0].length;
 
-        for (int y = 0; y < h; y ++) {
-            for (int x = 0; x < w; x ++) {
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
                 if (matrix[y][x] != 0) {
                     int px = (x + offsetx) * BLOCK_SIZE;
                     int py = (y + offsety) * BLOCK_SIZE;
                     rect.setBounds(px, py, px + BLOCK_SIZE, py + BLOCK_SIZE);
                     rect.draw(mCanvas);
-//                    mCanvas.drawRect(px, py, px + BLOCK_SIZE, py + BLOCK_SIZE, mPaint);
                 }
             }
         }
@@ -106,13 +100,30 @@ public class Block extends SurfaceView implements GestureDetector.OnGestureListe
 
     int[][] rotate(final int[][] block) {
         int[][] rotated = new int[block[0].length][];
-        for (int x = 0; x < block[0].length; x ++) {
+        for (int x = 0; x < block[0].length; x++) {
             rotated[x] = new int[block.length];
-            for (int y = 0; y < block.length; y ++) {
+            for (int y = 0; y < block.length; y++) {
                 rotated[x][block.length - y - 1] = block[y][x];
             }
         }
         return rotated;
+    }
+
+    boolean check(int[][] block, int offsetx, int offsety) {
+        if (offsetx < 0 || offsety < 0 ||
+                mapHeight - 1 < offsety + block.length ||
+                mapWidth - 1 < offsetx + block[0].length) {
+            Log.v("check", "true");
+            return false;
+        }
+        for (int y = 0; y < block.length; y++) {
+            for (int x = 0; x < block[y].length; x++) {
+                if (block[y][x] != 0 && map[y + offsety][x + offsetx] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     @Override
@@ -128,7 +139,9 @@ public class Block extends SurfaceView implements GestureDetector.OnGestureListe
     @Override
     public void onShowPress(MotionEvent motionEvent) {
         int[][] newBlock = rotate(block);
+        if (check(newBlock, posx, posy)) {
             block = newBlock;
+        }
     }
 
     @Override
@@ -150,23 +163,23 @@ public class Block extends SurfaceView implements GestureDetector.OnGestureListe
     public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent2, float v, float v2) {
         //フリック方向の判定
         if (motionEvent.getX() < motionEvent2.getX()) {
-            posx++;
+            if (check(block, posx + 1, posy)) {
+                posx++;
+            }
         } else {
-            posx--;
+            if (check(block, posx - 1, posy)) {
+                posx--;
+            }
         }
         return true;
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        Log.v("surfaceCreated","true");
-        mPaint = new Paint();
         mIsAttached = true;
         mThread = new Thread(this);
         mThread.start();
     }
-
-
 
     @Override
     public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
@@ -175,29 +188,79 @@ public class Block extends SurfaceView implements GestureDetector.OnGestureListe
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        Log.v("surfaceDestroyed","true");
         mIsAttached = false;
-        while (mThread.isAlive());
+        while (mThread.isAlive()) ;
 
     }
 
     @Override
     public void run() {
-        Log.v("run","true");
         while (mIsAttached) {
-            drawBlock(block, posx, posy, Color.RED);
+            mCanvas = getHolder().lockCanvas();
+            mCanvas.drawColor(Color.BLACK);
+            drawMatrix(block, posx, posy, Color.RED);
+            drawMatrix(map, 0, 0, Color.GRAY);
+            dropBlock();
             getHolder().unlockCanvasAndPost(mCanvas);
         }
     }
 
-    public static void finishLoop(){
-        synchronized (mThread){
+    public void dropBlock() {
+        try {
+            mThread.sleep(fallVelocity);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (check(block, posx, posy + 1)) {
+            posy++;
+        }
+    }
+
+    public static void finishLoop() {
+        synchronized (mThread) {
             mIsAttached = false;
         }
-            try {
-                mThread.join();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+        try {
+            mThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void setFallVelocity(int difficulty) {
+        fallVelocity = difficulty;
+    }
+
+    public void initGame() {
+        for (int y = 0; y < mapHeight; y++) {
+            map[y] = new int[mapWidth];
+            for (int x = 0; x < mapWidth; x++) {
+                //マップの外枠に色を塗る
+                if (y == mapHeight - 1 || x == mapWidth - 1
+                        || x == 0) {
+                    map[y][x] = 1;
+                }
             }
+        }
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        int y = posy;
+        while (check(block, posx, y)) {
+            y++;
+        }
+        if (y > 0) posy = y - 1;
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
     }
 }
--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/TetrisActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/TetrisActivity.java
@@ -3,39 +3,29 @@ package jp.ac.it_college.std.s13012.androidchallenge;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Display;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.widget.LinearLayout;
 
 
 public class TetrisActivity extends Activity{
-    int fallSpeed;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new Block(this));
 
+        //難易度によってブロックの落下速度を調整
         String difficulty = getIntent().getStringExtra("Difficulty");
         if (difficulty.equals("EASY")) {
-            fallSpeed = 3;
-
+            Block.setFallVelocity(1000);
         } else if (difficulty.equals("NORMAL")) {
-            fallSpeed = 5;
-
+            Block.setFallVelocity(800);
         } else if (difficulty.equals("HARD")) {
-            fallSpeed = 8;
-
+            Block.setFallVelocity(500);
         }
-
         Log.v("Difficulty", getIntent().getStringExtra("Difficulty"));
-        Log.v("FallSpeed", Integer.toString(fallSpeed));
     }
 
     @Override
     protected void onPause() {
+        //Blockクラスのスレッドを停止
         super.onPause();
         Block.finishLoop();
     }
