d4cbafb6816567a72d726b4330efb4d3e2a54816
s13012 <s13012@std.it-college.ac.jp>
2014-08-22 10:44:18 +0900

    layout-large復活

--- app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/NextBlock.java
+++ app/src/main/java/jp/ac/it_college/std/s13012/androidchallenge/NextBlock.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 public class NextBlock extends SurfaceView implements SurfaceHolder.Callback, Runnable {
     public static int mDifficulty;
     private TextView scoreText;
+    private TextView nextText;
     public static int score = 0;
     private static final int EASY = 0;
     private static final int NORMAL = 1;
